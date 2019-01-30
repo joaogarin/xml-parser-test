615771ccf7bf71a15a44538f491a02f4c9fa3a46
Joao Garin <joao.garin@gmail.com>
2019-01-30 10:23:16 +0000

    add docker

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var fs        = require('fs')
 var XmlStream = require('xml-stream') ;
-var stream=fs.createReadStream('feed_smaller.xml');
+var stream=fs.createReadStream('feed.xml');
 var xml = new XmlStream(stream);
-xml.preserve('title', true);
+// xml.preserve('title', true);
 //xml.collect('subitem');
 xml.on('endElement: title', function(item) {
   console.log(item);
